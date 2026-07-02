79e4ec648d8461d02746fb89ed2aafde7671b860
Yex Lawm <yexlawmveung@gmail.com>
2026-07-02 12:27:26 +0630

    file

---
+++ app/about/page.tsx
@@ -0,0 +1,15 @@
+export default function AboutPage(){
+    return(
+        <section className="max-w-4xl mx-auto p-8">
+            <h1 className="text-4xl font-bold mb-6">
+                About Me
+            </h1>
+            <p className="mb-4">
+                Hello, I'm Yex Lawm. I enjoy building Web application and Creative digital Projects.
+            </p>
+            <p>
+                My experience includes Next.js, Spring Boot, Python, Java, MySQL, and UI design.
+            </p>
+        </section>
+    );
+}
