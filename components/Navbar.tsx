558abc7a9e832c3dc7756f024c59e1784e33255d
Yex Lawm <yexlawmveung@gmail.com>
2026-07-03 10:12:10 +0630

    file

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,16 +1,19 @@
 import Link from "next/link";
 
-export default function Navbar(){
-    return(
-        <nav className="flex justify-between items-center p-6 shadow">
-            <h1 className="font-bold text-xl">Yex Lawm</h1>
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between items-center p-6 shadow">
+      <h1 className="font-bold text-xl">Yex Lawm</h1>
 
-            <div className="flex gap-6">
-                <link href=" /">Home</link>
-                <link href="/about">About</link>
-                <link href="/projects">Projects</link>
-                <link href="/contact">Contact</link>
-            </div>
-        </nav>
-    )
+      <div className="flex gap-6">
+        <Link href="/">Home</Link>
+
+        <Link href="/about">About</Link>
+
+        <Link href="/projects">Projects</Link>
+
+        <Link href="/contact">Contact</Link>
+      </div>
+    </nav>
+  );
 }
